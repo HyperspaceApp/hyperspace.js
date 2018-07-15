f4b7c0c693282df1482390ed8e325f91974d3ac4
Mark Huetsch <mark@hspace.app>
2018-07-15 22:32:41 +0800

    space cashify test

--- test/hyperspace.js
+++ test/hyperspace.js
@@ -2,7 +2,7 @@
 import 'babel-polyfill'
 import BigNumber from 'bignumber.js'
 import Path from 'path'
-import { agent, siacoinsToHastings, call, hastingsToSiacoins, isRunning, connect, errCouldNotConnect } from '../src/hyperspace.js'
+import { agent, spaceCashToHastings, call, hastingsToSpaceCash, isRunning, connect, errCouldNotConnect } from '../src/hyperspace.js'
 import http from 'http'
 import readdir from 'readdir'
 import { expect } from 'chai'
@@ -29,36 +29,36 @@ const { launch, makeRequest } = proxyquire('../src/hyperspace.js', mock)
 
 BigNumber.config({DECIMAL_PLACES: 28})
 
-const hastingsPerSiacoin = new BigNumber('1000000000000000000000000')
+const hastingsPerSpaceCash = new BigNumber('1000000000000000000000000')
 
 describe('hyperspace.js wrapper library', () => {
 	describe('unit conversion functions', () => {
-		it('converts from siacoins to hastings correctly', () => {
+		it('converts from space cash to hastings correctly', () => {
 			const maxSC = new BigNumber('100000000000000000000000')
 			for (let i = 0; i < 999; i++) {
 				const sc = maxSC.times(Math.trunc(Math.random() * 100000) / 100000)
-				const expectedHastings = sc.times(hastingsPerSiacoin)
-				expect(siacoinsToHastings(sc).toString()).to.equal(expectedHastings.toString())
+				const expectedHastings = sc.times(hastingsPerSpaceCash)
+				expect(spaceCashToHastings(sc).toString()).to.equal(expectedHastings.toString())
 			}
 		})
-		it('converts from hastings to siacoins correctly', () => {
+		it('converts from hastings to space cash correctly', () => {
 			const maxH = new BigNumber('10').toPower(150)
 			for (let i = 0; i < 999; i++) {
 				const h = maxH.times(Math.trunc(Math.random() * 100000) / 100000)
-				const expectedSiacoins = h.dividedBy(hastingsPerSiacoin)
-				expect(hastingsToSiacoins(h).toString()).to.equal(expectedSiacoins.toString())
+				const expectedSpaceCash = h.dividedBy(hastingsPerSpaceCash)
+				expect(hastingsToSpaceCash(h).toString()).to.equal(expectedSpaceCash.toString())
 			}
 		})
 		it('does not lose precision during unit conversions', () => {
-			// convert from base unit -> siacoins n_iter times, comparing the (n_iter-times) converted value at the end.
+			// convert from base unit -> space cash n_iter times, comparing the (n_iter-times) converted value at the end.
 			// if precision loss were occuring, the original and the converted value would differ.
 			const n_iter = 10000
-			const originalSiacoin = new BigNumber('1337338498282837188273')
-			let convertedSiacoin = originalSiacoin
+			const originalSpaceCash = new BigNumber('1337338498282837188273')
+			let convertedSpaceCash = originalSpaceCash
 			for (let i = 0; i < n_iter; i++) {
-				convertedSiacoin = hastingsToSiacoins(siacoinsToHastings(convertedSiacoin))
+				convertedSpaceCash = hastingsToSpaceCash(spaceCashToHastings(convertedSpaceCash))
 			}
-			expect(convertedSiacoin.toString()).to.equal(originalSiacoin.toString())
+			expect(convertedSpaceCash.toString()).to.equal(originalSpaceCash.toString())
 		})
 	})
 	describe('hsd interaction functions', () => {
